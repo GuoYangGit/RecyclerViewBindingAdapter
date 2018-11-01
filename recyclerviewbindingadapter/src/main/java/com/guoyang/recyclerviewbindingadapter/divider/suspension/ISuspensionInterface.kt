b9a8288f2d6c492b5e94d3e8bbf5b45be5cb4d8a
郭杨 <guoyang_add@163.com>
2018-11-01 12:57:38 +0800

    加入悬停的索引布局

---
+++ recyclerviewbindingadapter/src/main/java/com/guoyang/recyclerviewbindingadapter/divider/suspension/ISuspensionInterface.kt
@@ -0,0 +1,33 @@
+package com.guoyang.recyclerviewbindingadapter.divider.suspension
+
+/***
+ * You may think you know what the following code does.
+ * But you dont. Trust me.
+ * Fiddle with it, and youll spend many a sleepless
+ * night cursing the moment you thought youd be clever
+ * enough to "optimize" the code below.
+ * Now close this file and go play with something else.
+ */
+/***
+ *
+ *   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ *  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ *  ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ *  ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ *           ░     ░ ░      ░  ░
+ *
+ * Created by guoyang on 2018/10/30.
+ * github https://github.com/GuoYangGit
+ * QQ:352391291
+ */
+interface ISuspensionInterface {
+    //是否需要显示悬停title
+    fun isShowSuspension():Boolean
+
+    //悬停的title
+    fun getSuspensionTag(): String
+}
